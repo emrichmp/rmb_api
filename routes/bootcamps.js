98cb5c98d677544da49ff484758a916eb076db1e
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-08-12 14:35:42 -0700

    init

---
+++ routes/bootcamps.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+const Bootcamp = require('../models/bootcamp')
+
+//Get all
+router.get('/', async (req, res) => {
+    //res.send('Hello')
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.json(bootcamps)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+//Get one
+router.get('/:id', getBootcamp, (req, res) => {
+    res.send(res.bootcamp)
+})
+//Create one
+router.post('/', async (req, res) => {
+    const bootcamp = new Bootcamp({
+        name: req.body.name,
+        date: req.body.date
+    })
+    try {
+        const newBootcamp = await bootcamp.save()
+        res.status(201).json(newBootcamp)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+//Update one
+router.patch('/:id', getBootcamp, async (req, res) => {
+    if (req.body.name != null) {
+        res.bootcamp.name = req.body.name
+    }
+    if (req.body.date != null) {
+        res.bootcamp.date = req.body.date
+    }
+    try {
+        const updatedBootcamp = await res.bootcamp.save()
+        res.json(updatedBootcamp)
+    } catch (err) {
+        res.status(400).json({ message: err.message})
+    }
+})
+//Delete one
+router.delete('/:id', getBootcamp, async (req, res) => {
+    try {
+        await res.bootcamp.remove()
+        res.json({ message: "Deleted bootcamp" })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+async function getBootcamp(req, res, next) {
+    let bootcamp
+    try {
+        bootcamp = await Bootcamp.findById(req.params.id)
+        if (bootcamp === null) {
+            return res.status(404).json({ message: "can't find bootcamp" })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.bootcamp = bootcamp
+    next()
+}
+
+module.exports = router
